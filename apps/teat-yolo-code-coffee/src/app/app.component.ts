3d8596d6e2fdb8e7154a2e31e49605793e59f749
Charlie Greenman <charlie@razroo.com>
2023-09-10 19:16:40 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/teat-yolo-code-coffee/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teat-yolo-code-coffee-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'teat-yolo-code-coffee';
+}
